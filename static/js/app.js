8718e0a651c03699b7e22733a7c1ecbf83648845
Hsuan-Yu Lin <o100249243@gmail.com>
2019-03-12 15:08:33 -0700

    Level 2 completed

--- static/js/app.js
+++ static/js/app.js
@@ -6,22 +6,92 @@ var submit = d3.select("#filter-btn");
 
 submit.on("click", function () {
 
+    // prevent refreshing
     d3.event.preventDefault();
 
+    // clear the table
     d3.select("tbody").selectAll("*").remove();
 
+    // get the date value from input
+    var date = d3.select("#datetime");
+    var dateValue = date.property("value");
+    console.log(dateValue);
 
-    var input = d3.select("#datetime");
-    var inputValue = input.property("value");
-    console.log(inputValue);
+    // get the city value from input
+    var city = d3.select("#cityname");
+    var cityValue = city.property("value");
+    console.log(cityValue);
 
-    function filtered(tableData) {
-        return tableData.datetime === inputValue;
+    // get the state value from input
+    var state = d3.select("#statename");
+    var stateValue = state.property("value");
+    console.log(stateValue);
+
+    // get the country value from input
+    var country = d3.select("#countryname");
+    var countryValue = country.property("value");
+    console.log(countryValue);
+
+    // get the shape value from input
+    var shape = d3.select("#shapename");
+    var shapeValue = shape.property("value");
+    console.log(shapeValue);
+
+    // date filter
+    function datefilter(tableData) {
+        if (dateValue != "") {
+            return tableData.datetime === dateValue;
+        } else {
+            return true;
+        }
     }
 
-    var filtered_data = tableData.filter(filtered);
+    // city filter
+    function cityfilter(tableData) {
+        if (cityValue != "") {
+            return tableData.city === cityValue;
+        } else {
+            return true;
+        }
+    }
+
+    // state filter
+    function statefilter(tableData) {
+        if (stateValue != "") {
+            return tableData.state === stateValue;
+        } else {
+            return true;
+        }
+    }
+
+    // country filter
+    function countryfilter(tableData) {
+        if (countryValue != "") {
+            return tableData.country === countryValue;
+        } else {
+            return true;
+        }
+    }
+
+    // shape filter
+    function shapefilter(tableData) {
+        if (shapeValue != "") {
+            return tableData.shape === shapeValue;
+        } else {
+            return true;
+        }
+    }
+
+    // get the filtered data
+    var filtered_data = tableData.
+        filter(datefilter).
+        filter(cityfilter).
+        filter(statefilter).
+        filter(countryfilter).
+        filter(shapefilter);
     console.log(filtered_data);
 
+    // indert the filtered data into html
     var tbody = d3.select("tbody");
     filtered_data.forEach(function (ufo) {
         var row = tbody.append("tr");
